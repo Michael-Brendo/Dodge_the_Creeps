55e8a21a15e30e94175b2f895545ed888b975164
Michael-Brendo <Michael-Brendo@users.noreply.github.com>
2022-10-12 14:06:14 -0300

    Added move command to player.

--- Player.gd
+++ Player.gd
@@ -22,4 +22,15 @@ func _process(delta):
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
 		
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	
+	else:
+		$AnimatedSprite.stop()
+		
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+	
 	 
